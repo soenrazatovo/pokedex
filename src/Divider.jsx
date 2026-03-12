ee6065b03387461e068e1c933e86c73a34cec199
Soen Razatovo <soen.razatovo@gmail.com>
2026-03-12 23:13:58 +0100

    :construction: Divider overlap other when clicked

--- src/Divider.jsx
+++ src/Divider.jsx
@@ -1,4 +1,7 @@
-function Divider({width, height, borderRadius}) {
+import { useState } from "react"
+import "./Divider.css"
+
+function Divider({width, height, borderRadius, text = "", textSize, textColor = "black", backgroundColor, borderColor = "black", borderSize = "0px", topCorner = true, bottomCorner = true, zIndex = 0, onClick}) {
     width = width.toString().replace("px","")
     height = height.toString().replace("px","")
     
@@ -8,30 +11,51 @@ function Divider({width, height, borderRadius}) {
             borderRadius = Math.min(width/2,height/4)
         }
     } else {
-        borderRadius = Math.min(width,height)/4
+        borderRadius = Math.min(width/2,height/4)
     }
 
     return(
         <>
             { (width >= borderRadius*2 && height >= borderRadius*4 && borderRadius != undefined && borderRadius != "") &&
-                <svg width={width + "px"} height={height + "px"} >
-                    <path d={
-                    `
-                    M ${width},${0}
-                    Q ${width},${borderRadius} ${width-borderRadius},${borderRadius} 
-                    L ${borderRadius},${borderRadius} 
-                    Q ${0},${borderRadius} ${0},${borderRadius*2} 
-                    L ${0},${height-borderRadius*2}
-                    Q ${0},${height-borderRadius} ${borderRadius},${height-borderRadius}
-                    L ${width-borderRadius},${height-borderRadius}
-                    Q ${width},${height-borderRadius} ${width},${height}       
-                    `
-                    }
-                    style={{fill: "#e26767"}} 
-                    stroke={"#DD0000"}
-                    strokeWidth={1}
-                    ></path>
-                </svg>
+                <div onClick={onClick} className={"divider-wrapper"} style={{width : width + "px", height : height-borderRadius*2 + "px", zIndex : zIndex}}>
+                    <svg className={"divider-svg"} style={{width : width + "px", height : height + "px"}}>
+                        <path d={
+                        (topCorner ?
+                            `
+                            M ${width},${0}
+                            Q ${width},${borderRadius} ${width-borderRadius},${borderRadius} 
+                            `
+                        :
+                            `
+                            M ${width},${borderRadius}
+                            `
+                        )
+                        +
+                        `
+                        L ${borderRadius},${borderRadius} 
+                        Q ${0},${borderRadius} ${0},${borderRadius*2} 
+                        L ${0},${height-borderRadius*2}
+                        Q ${0},${height-borderRadius} ${borderRadius},${height-borderRadius}
+                        `
+                        +
+                        (bottomCorner ?
+                            `
+                            L ${width-borderRadius},${height-borderRadius}
+                            Q ${width},${height-borderRadius} ${width},${height}       
+                            `
+                        :
+                            `
+                            L ${width},${height-borderRadius}
+                            `
+                        )
+                        }
+                        style={{fill: backgroundColor}} 
+                        stroke={borderColor}
+                        strokeWidth={borderSize}
+                        ></path>
+                    </svg>
+                    <p className={"divider-text"} style={{fontSize: textSize, color: textColor}}>{text}</p>
+                </div>
             }
         </>
     )
